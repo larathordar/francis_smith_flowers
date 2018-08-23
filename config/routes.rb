91e9594b80f256d4774ea1065147fbfe24059c06
larathordar <lara-thordar@hotmail.com>
2018-08-23 08:53:04 +0200

    front

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get 'flowers/home'
   get 'landing/index'
   root controller: :landing, action: :index
+  match '/contacts',     to: 'contacts#new',             via: 'get'
+resources "contacts", only: [:new, :create]
 end
