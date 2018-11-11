1493448825ba45ed7d223d3ae8dddce896f953ef
larathordar <lara-thordar@hotmail.com>
2018-11-11 20:55:19 +0100

    fixed as requested

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -2,3 +2,11 @@
 setTimeout(function() {
   $('#myModal').modal();
 }, 1500);
+
+$("nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+});
