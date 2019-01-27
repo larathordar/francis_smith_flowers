17eedd657c4f7f82f43f264467241d6e92e1edb0
larathordar <lara-thordar@hotmail.com>
2019-01-27 08:52:05 +0100

    fix nav

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,3 +2,10 @@ class LandingController < ApplicationController
   def index
   end
 end
+
+class CookieController < ActionController::Base
+  def cookie
+    # put any code here that you need
+    # (although for a static view you probably won't have any)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'pricing/weekly'
   get 'pricing/monthly'
   get 'pricing/fortnightly'
+  get 'landing/cookie'
+
 
 
 
